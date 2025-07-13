cbc6d070926ce0bacaa574283dd1c4b8aa12c676
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-07-13 14:08:51 +0800

    2025年7月13日

--- 5-ADMIXTURE/script/1-1-最优K.sh
+++ 5-ADMIXTURE/script/1-1-最优K.sh
@@ -1,1 +1,21 @@
-grep 'CV error' ./*.log > result.txt
+#*======== 从ADMIXTURE的LOG文件中获取最优K =======
+#*==============================================
+LOG_DIR='/home/luolintao/Helicopter/Script/分析结果/ADMIXTURE/data/'
+PREFIX='ADMIXTURE_7544_filtered'
+OUTPUT_FILE="${LOG_DIR}/cv_results.csv"
+
+# 创建CSV文件并添加标题行
+echo "K,CV" > $OUTPUT_FILE
+
+# 进入日志目录
+cd $LOG_DIR
+# 获取所有以PREFIX开头的日志文件
+LOG_FILES=$(ls ${PREFIX}*.log)
+# 遍历每个日志文件
+for LOG_FILE in $LOG_FILES; do
+    # 提取K值和CV error值并格式化为CSV
+    grep 'CV error' "$LOG_FILE" | sed -E 's/CV error \(K=([0-9]+)\): ([0-9.]+)/\1,\2/' >> $OUTPUT_FILE
+done
+# 将结果按K值排序
+sort -n -t ',' -k1 $OUTPUT_FILE -o $OUTPUT_FILE
+echo "CSV结果已保存到 $OUTPUT_FILE 文件中"
--- 5-ADMIXTURE/script/1-2-可视化.sh
+++ 5-ADMIXTURE/script/1-2-可视化.sh
@@ -1,1 +1,3 @@
-Rscript 可视化.R 文件的前缀（在这里是ADMIXTURE）
+Rscript  \
+    /home/luolintao/S00-Github/3-Autosomal/5-ADMIXTURE/func/plot_strucAverage.R\
+    /home/luolintao/Helicopter/Script/分析结果/ADMIXTURE/data/ADMIXTURE_7544_filtered
