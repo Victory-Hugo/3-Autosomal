9dddc51df7867b3b4fa2770915d70cd5802a4477
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-07-13 16:47:37 +0800

    2025年7月13日

--- 1-treemix/script/2-treemix-parallel.sh
+++ 1-treemix/script/2-treemix-parallel.sh
@@ -2,11 +2,11 @@
 # 生成16个独立的TreeMix运行脚本，对应迁移边数量m从0到15，每种m做10次重复
 #*===============================
 # todo 设定输入文件路径，输入文件通过./python/plink2treemix.py生成
-INPUT_FILE="/home/luolintao/Helicopter/Script/分析结果/treemix/data/TreeMix.gz"
+INPUT_FILE="/home/luolintao/Helicopter/Script/分析结果/treemix/data/TreeMix_China3622.gz"
 # todo 设定输出目录
-OUTPUT_DIR="/home/luolintao/Helicopter/Script/分析结果/treemix/output/global/"
+OUTPUT_DIR="/home/luolintao/Helicopter/Script/分析结果/treemix/output/China/"
 # todo 设定脚本输出目录
-SCRIPT_DIR="/home/luolintao/Helicopter/Script/分析结果/treemix/script/global/"
+SCRIPT_DIR="/home/luolintao/Helicopter/Script/分析结果/treemix/script/China/"
 # todo 设定outgroup
 OUTGROUP="hpAfrica2"
 #*===============================
