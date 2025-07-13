d2b6795a1e8c1bf9d0b346f75c5a73d26ef5ee09
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-07-13 16:16:18 +0800

    2025年7月13日

--- 1-treemix/script/2-SQX-可视化.R
+++ 1-treemix/script/2-SQX-可视化.R
@@ -1,7 +1,7 @@
 library(OptM)
 # 如果文件前缀不是"global"，请修改为实际的前缀
 # 分析rep1的数据
-linear_rep1 = optM("/home/luolintao/Helicopter/Script/分析结果/treemix/output")
-plot_optM(linear_rep1, main="Rep1")
+linear = optM("/home/luolintao/Helicopter/Script/分析结果/treemix/output")
+plot_optM(linear)
 
 # 如果有rep2，可以类似分析
