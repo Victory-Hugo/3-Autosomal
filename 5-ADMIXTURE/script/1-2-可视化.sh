38391d863b3f7588dfbabd10d37f25c6b8c7b8ba
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-07-13 14:22:55 +0800

    2025年7月13日

--- 5-ADMIXTURE/script/1-2-可视化.sh
+++ 5-ADMIXTURE/script/1-2-可视化.sh
@@ -1,3 +1,3 @@
 Rscript  \
-    /home/luolintao/S00-Github/3-Autosomal/5-ADMIXTURE/func/plot_strucAverage.R\
+    /home/luolintao/S00-Github/3-Autosomal/5-ADMIXTURE/func/plot_strucAverage.R \
     /home/luolintao/Helicopter/Script/分析结果/ADMIXTURE/data/ADMIXTURE_7544_filtered
