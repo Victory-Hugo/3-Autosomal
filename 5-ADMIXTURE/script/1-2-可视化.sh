05453ca6061a53d0a308afdaf9ca28bad8195d63
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-07-19 14:14:46 +0800

    2025年7月19日

--- 5-ADMIXTURE/script/1-2-可视化.sh
+++ 5-ADMIXTURE/script/1-2-可视化.sh
@@ -1,3 +1,3 @@
 Rscript  \
-    /home/luolintao/S00-Github/3-Autosomal/5-ADMIXTURE/func/plot_strucAverage.R \
-    /home/luolintao/Helicopter/Script/分析结果/ADMIXTURE/data/ADMIXTURE_7544_filtered
+    /mnt/f/OneDrive/文档（科研）/脚本/Download/3-Autosomal/5-ADMIXTURE/func/plot_strucAverage.R \
+    /mnt/d/幽门螺旋杆菌/Script/分析结果/ADMIXTURE/data/WGS.aln.fasta.CDS_地理东亚及周边_3378_filtered
