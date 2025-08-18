c853a25b23680da90db98c2e42fbd55c73dde469
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-08-18 09:09:23 +0800

    rq2

--- 4-fineSTRCTURE/script/8-fs聚类树美化.R
+++ 4-fineSTRCTURE/script/8-fs聚类树美化.R
@@ -77,8 +77,8 @@ sub_tree_grouped <- groupOTU(sub_tree, group_list)
 p3 <- ggtree(sub_tree_grouped, aes(color = group), layout = 'dendrogram',  lwd = 0.2) + #* 如果没有分支着色请删除aes(color = group))
   scale_color_manual(values = group_colors) + #* 如果没有分支着色请删除scale_color_manual(values = group_colors)
   geom_tiplab(size = 0.5, align = TRUE, hjust = -0.5) + #* 添加tiplab，默认显示`label`
-  #geom_nodepoint(size = 0.1) +   #* 添加nodepoint
-  # geom_nodelab(aes(label = round(branch.length, 3)), hjust = -0.3, size = 0.7) + #* 显示branch.length
+  geom_nodepoint(size = 0.1) +   #* 添加nodepoint
+  #geom_nodelab(aes(label = round(branch.length, 3)), hjust = -0.3, size = 0.7) + #* 显示branch.length
   #geom_nodelab(aes(label = node),size = 0.7) + #* 显示branch.length +
   theme(
     axis.text.x = element_text(size = 2),    #* x轴刻度文字大小
@@ -102,24 +102,27 @@ library(ggtreeExtra)
 library(ggnewscale)
 library(MetBrewer)
 # 读取数据
-df_meta <- read_csv("Presence_absence.csv")
-df_meta
+df_cagA <- read_csv("CagA_Presence_absence.csv")
+df_VacA <- read_csv("VacA_Presence_absence.csv")
+df_Heptan <- read_csv("Heptan_T_Presence_absence.csv")
 
-df_meta -> df_meta_long
+df_Heptan |>mutate(as.factor(Heptan_T)) -> df_Heptan_long
+df_VacA  -> df_VacA_long
+df_cagA  -> df_cagA_long
 
-df_meta_long
+
 # 检查CagA_subtype的唯一值数量
-unique_subtypes <- length(unique(df_meta_long$CagA_subtype))
+unique_subtypes <- length(unique(df_cagA_long$CagA_subtype))
 cat("CagA_subtype 唯一值数量:", unique_subtypes, "\n")
 
 # 解决方案1: 使用更多颜色的色板
 p4 <- p3 + 
     new_scale_fill() +
     geom_fruit(
-      data = df_meta_long,
+      data = df_cagA_long,
       geom = geom_tile,
       mapping = aes(y = label, fill = CagA_subtype),
-      width = 0.2,
+      pwidth = 0.2,
       offset = 0.01,
       axis.params = list(axis = "x", text.angle = -45, hjust = 0)
     ) + 
@@ -131,132 +134,128 @@ p4 <- p3 +
     labs(fill = "CagA Subtype")  #! 自定义图例标题
 p4
 
-ggsave(p4, filename = '系统发育树_汉藏_cagA.pdf', units = "cm", height = 10, width = 20)
+# 检查df_VacA_long的唯一值数量
+unique_subtypes <- length(unique(df_VacA_long$VacA_subtype))
+cat("df_VacA_long 唯一值数量:", unique_subtypes, "\n")
 
-library(ggtree)
-library(ggtreeExtra)
-library(tidyverse)
-library(ggstar)
-library(ggnewscale)
 
-# 树文件路径
-trfile <- system.file("extdata", "tree.nwk", package = "ggtreeExtra")
-# 提示点数据文件路径
-tippoint1 <- system.file("extdata", "tree_tippoint_bar.csv", package = "ggtreeExtra")
-# 第一层环数据文件路径
-ring1 <- system.file("extdata", "first_ring_discrete.csv", package = "ggtreeExtra")
-# 第二层环数据文件路径
-ring2 <- system.file("extdata", "second_ring_continuous.csv", package = "ggtreeExtra")
 
-# 使用 read.tree 导入树文件
-tree <- read.tree(trfile)
-
-# 读取注释数据
-dat1 <- read.csv(tippoint1)
-knitr::kable(head(dat1))
-dat2 <- read.csv(ring1)
-knitr::kable(head(dat2))
-dat3 <- read.csv(ring2)
-knitr::kable(head(dat3))
-
-# 绘制基础树
-p <- ggtree(tree, layout = "fan", open.angle = 10, size = 0.5)
-
-# 星状点图层
-p2 <- p +
-  geom_fruit(
-    data = dat1,
-    geom = geom_star,
-    mapping = aes(
-      y = ID,
-      fill = Location,
-      size = Length,
-      starshape = Group
-    ),
-    position = "identity",
-    starstroke = 0.5,
-    color = "#FFFFFF"
-  ) +
-  scale_size_continuous(
-    range = c(1, 3),
-    guide = guide_legend(
-      keywidth = 0.5,
-      keyheight = 0.5,
-      override.aes = list(starshape = 15),
-      order = 2
-    )
-  ) +
-  scale_fill_manual(
-    values = c(
-      "#F8766D", "#C49A00", "#53B400",
-      "#00C094", "#00B6EB", "#A58AFF", "#FB61D7"
-    ),
-    guide = "none"
-  ) +
-  scale_starshape_manual(
-    values = c(1, 15),
-    guide = guide_legend(keywidth = 0.5, keyheight = 0.5, order = 1)
-  )
-
-# 第一层离散热图
-p3 <- p2 +
-  new_scale_fill() +
-  geom_fruit(
-    data = dat2,
-    geom = geom_tile,
-    mapping = aes(y = ID, x = Pos, fill = Type),
-    offset = 0.08,
-    colour = "white",
-    linewidth = 0.2,
-    pwidth = 0.25,
-    axis.params = list(axis = "x", text.angle = -45, hjust = 0)
-  ) +
-  scale_fill_manual(
-    values = c("#339933", "#dfac03"),
-    guide = guide_legend(keywidth = 0.5, keyheight = 0.5, order = 3)
-  )
-
-# 第二层连续热图
-p4 <- p3 +
-  new_scale_fill() +
-  geom_fruit(
-    data = dat3,
-    geom = geom_tile,
-    mapping = aes(y = ID, x = Type2, alpha = Alpha, fill = Type2),
-    pwidth = 0.15,
-    axis.params = list(axis = "x", text.angle = -45, hjust = 0)
-  ) +
-  scale_fill_manual(
-    values = c("#b22222", "#005500", "#0000be", "#9f1f9f"),
-    guide = guide_legend(keywidth = 0.5, keyheight = 0.5, order = 4)
-  ) +
-  scale_alpha_continuous(
-    range = c(0, 0.4),
-    guide = guide_legend(keywidth = 0.5, keyheight = 0.5, order = 5)
-  )
-
-# 条形图层
-p5 <- p4 +
-  new_scale_fill() +
-  geom_fruit(
-    data = dat1,
-    geom = geom_col,
-    mapping = aes(y = ID, x = Abundance, fill = Location),
-    pwidth = 0.4,
-    axis.params = list(axis = "x", text.angle = -45, hjust = 0)
-  ) +
-  scale_fill_manual(
-    values = c(
-      "#F8766D", "#C49A00", "#53B400",
-      "#00C094", "#00B6EB", "#A58AFF", "#FB61D7"
-    ),
-    guide = guide_legend(keywidth = 0.5, keyheight = 0.5, order = 6)
-  ) +
-  theme(
-    legend.background = element_rect(fill = NA),
-    legend.title = element_text(size = 7),
-    legend.text = element_text(size = 6),
-    legend.spacing.y = unit(0.02, "cm")
-  )
-
+p5 <- p4 + 
+    new_scale_fill() +
+    geom_fruit(
+      data = df_VacA_long,
+      geom = geom_tile,
+      mapping = aes(y = label, fill = VacA_subtype),
+      pwidth = 0.2,
+      offset = 0.04,
+      axis.params = list(axis = "x", text.angle = -45, hjust = 0)
+    ) + 
+    scale_fill_manual(values = c(
+      met.brewer("Homer1", n = 5, override.order = FALSE, direction = -1)  # 如果还不够用彩虹色
+    )) + 
+    labs(fill = "VacA Subtype")  #! 自定义图例标题
 p5
+
+
+# 检查df_Heptan_long的唯一值数量
+unique_subtypes <- length(unique(df_Heptan_long$`as.factor(Heptan_T)`))
+cat("df_Heptan_long 唯一值数量:", unique_subtypes, "\n")
+
+p6 <- p5 + 
+    new_scale_fill() +
+    geom_fruit(
+      data = df_Heptan_long,
+      geom = geom_tile,
+      mapping = aes(y = label, fill = `as.factor(Heptan_T)`),
+      pwidth = 0.2,
+      offset = 1.1,
+      axis.params = list(axis = "x", text.angle = -45, hjust = 0)
+    ) + 
+    scale_fill_manual(values = c(
+      met.brewer("Isfahan1", n = 2, override.order = FALSE, direction = -1)  # 如果还不够用彩虹色
+    )) + 
+    labs(fill = "Heptan_T Presence/Absence")  #! 自定义图例标题
+
+p6 
+
+ggsave(p6, filename = '系统发育树_汉藏_p6.pdf', units = "cm", height = 10, width = 20)
+
+
+# 检查CagA_subtype的唯一值数量
+unique_subtypes <- length(unique(df_cagA_long$CagA_subtype))
+cat("CagA_subtype 唯一值数量:", unique_subtypes, "\n")
+
+# 解决方案1: 使用更多颜色的色板
+p7 <- p0 + 
+    new_scale_fill() +
+    geom_fruit(
+      data = df_cagA_long,
+      geom = geom_tile,
+      mapping = aes(y = label, fill = CagA_subtype),
+      pwidth = 0.2,
+      offset = 0.01,
+      axis.params = list(axis = "x", text.angle = -45, hjust = 0)
+    ) + 
+    scale_fill_manual(values = c(
+      met.brewer("Troy", n = min(unique_subtypes, 16)),  
+      met.brewer("Cassatt2", n = max(0, unique_subtypes - 5)), 
+      rainbow(max(0, unique_subtypes - 24))  # 如果还不够用彩虹色
+    )) + 
+    labs(fill = "CagA Subtype")  #! 自定义图例标题
+p7
+
+
+
+
+# 检查df_VacA_long的唯一值数量
+unique_subtypes <- length(unique(df_VacA_long$VacA_subtype))
+cat("df_VacA_long 唯一值数量:", unique_subtypes, "\n")
+
+
+
+p8 <- p7 + 
+    new_scale_fill() +
+    geom_fruit(
+      data = df_VacA_long,
+      geom = geom_tile,
+      mapping = aes(y = label, fill = VacA_subtype),
+      pwidth = 0.2,
+      offset = 0.04,
+      axis.params = list(axis = "x", text.angle = -45, hjust = 0)
+    ) + 
+    scale_fill_manual(values = c(
+      met.brewer("Homer1", n = 18, override.order = FALSE, direction = -1)  # 如果还不够用彩虹色
+    )) + 
+    labs(fill = "VacA Subtype")  #! 自定义图例标题
+p8
+
+
+# 检查df_Heptan_long的唯一值数量
+unique_subtypes <- length(unique(df_Heptan_long$`as.factor(Heptan_T)`))
+cat("df_Heptan_long 唯一值数量:", unique_subtypes, "\n")
+
+p9 <- p8 + 
+    new_scale_fill() +
+    geom_fruit(
+      data = df_Heptan_long,
+      geom = geom_tile,
+      mapping = aes(y = label, fill = `as.factor(Heptan_T)`),
+      pwidth = 0.2,
+      offset = 1.1,
+      axis.params = list(axis = "x", text.angle = -45, hjust = 0)
+    ) + 
+    scale_fill_manual(values = c(
+      met.brewer("Isfahan1", n = 2, override.order = FALSE, direction = -1)  # 如果还不够用彩虹色
+    )) + 
+    labs(fill = "Heptan_T Presence/Absence")  #! 自定义图例标题
+
+p9
+ggsave(p9, filename = '系统发育树_全球_p9.pdf', units = "cm", height = 10, width = 20)
+
+library(patchwork)
+
+p6 / p9 + plot_layout(ncol = 1, guides = "collect") + 
+  plot_annotation(tag_levels = 'A') & 
+  theme(legend.position = 'bottom')  -> p_all
+
+ggsave(p_all, filename = '系统发育树_全球_汉藏.pdf', units = "cm", height = 20, width = 20)
